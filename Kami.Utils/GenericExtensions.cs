c44783faef53fd4be852a233e5c7e0fb55e9f095
Dan <dan.mok999@yandex.ru>
2023-05-25 17:26:34 +0500

    Use any function as extension

---
+++ Kami.Utils/GenericExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kami.Utils;
+
+public static class GenericExtensions
+{
+    /// <summary>
+    /// Allows using any function as extension method to chain calls
+    /// </summary>
+    public static TResult Chain<TSource, TResult>(this TSource source, Func<TSource, TResult> extension) => extension(source);
+}
