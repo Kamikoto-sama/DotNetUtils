c5db61fb3518f52ccee77231115754e1fc7ccc92
Dan <dan.mok999@yandex.ru>
2023-05-26 13:28:05 +0500

    .AddToSet

--- Kami.Utils/DictionaryExtensions.cs
+++ Kami.Utils/DictionaryExtensions.cs
@@ -15,8 +15,29 @@ public static class DictionaryExtensions
 
     public static void AddToList<TKey, TValue>(this IDictionary<TKey, List<TValue>> dictionary, TKey key, TValue value)
     {
-        if (!dictionary.ContainsKey(key))
-            dictionary[key] = new List<TValue>();
-        dictionary[key].Add(value);
+        if (!dictionary.TryGetValue(key, out var list))
+            dictionary[key] = list = new List<TValue>();
+        list.Add(value);
+    }
+
+    public static void AddToList<TKey, TValue>(this IDictionary<TKey, List<TValue>> dictionary, TKey key, IEnumerable<TValue> values)
+    {
+        if (!dictionary.TryGetValue(key, out var list))
+            dictionary[key] = list = new List<TValue>();
+        list.AddRange(values);
+    }
+
+    public static bool AddToSet<TKey, TValue>(this Dictionary<TKey, HashSet<TValue>> dictionary, TKey key, TValue value) where TKey : notnull
+    {
+        if (!dictionary.TryGetValue(key, out var set))
+            dictionary[key] = set = new HashSet<TValue>();
+        return set.Add(value);
+    }
+
+    public static void AddToSet<TKey, TValue>(this Dictionary<TKey, HashSet<TValue>> dictionary, TKey key, IEnumerable<TValue> values) where TKey : notnull
+    {
+        if (!dictionary.TryGetValue(key, out var set))
+            dictionary[key] = set = new HashSet<TValue>();
+        set.AddRange(values);
     }
 }
