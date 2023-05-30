e1106e58e1f52008b71ba8bfb61e309c446e178d
Dan <dan.mok999@yandex.ru>
2023-05-30 19:10:28 +0500

    AsyncLock

---
+++ Kami.Utils/AsyncLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kami.Utils;
+
+public class AsyncLock
+{
+    private readonly SemaphoreSlim semaphore = new(1, 1);
+
+    public Task<IDisposable> Obtain() => Obtain(TimeSpan.MaxValue, default);
+
+    public Task<IDisposable> Obtain(CancellationToken ct) => Obtain(Timeout.InfiniteTimeSpan, ct);
+
+    public Task<IDisposable> Obtain(TimeSpan timeout) => Obtain(timeout, default);
+
+    public async Task<IDisposable> Obtain(TimeSpan timeout, CancellationToken ct)
+    {
+        await semaphore.WaitAsync(timeout, ct);
+        return new LockHolder(semaphore);
+    }
+
+    private readonly struct LockHolder : IDisposable
+    {
+        private readonly SemaphoreSlim semaphore;
+
+        public LockHolder(SemaphoreSlim semaphore)
+        {
+            this.semaphore = semaphore;
+        }
+
+        public void Dispose() => semaphore.Release();
+    }
+}
