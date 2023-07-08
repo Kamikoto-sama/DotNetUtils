9a0e80f1f4d456d8462ed85b4ddcbc1ea4ee7dfd
Dan <38423206+Kamikoto-sama@users.noreply.github.com>
2023-07-08 13:19:00 +0500

    TupleUtils: Deconstruct 3 and 4

--- Kami.Utils/TupleUtils.cs
+++ Kami.Utils/TupleUtils.cs
@@ -7,9 +7,29 @@ public static class TupleUtils
 {
     public static void Deconstruct<T>(this IEnumerable<T> source, out T item0, out T item1)
     {
+        const int count = 2;
         using var enumerator = source.GetEnumerator();
-        item0 = GetNextElement(enumerator, 2);
-        item1 = GetNextElement(enumerator, 2);
+        item0 = GetNextElement(enumerator, count);
+        item1 = GetNextElement(enumerator, count);
+    }
+
+    public static void Deconstruct<T>(this IEnumerable<T> source, out T item0, out T item1, out T item2)
+    {
+        const int count = 3;
+        using var enumerator = source.GetEnumerator();
+        item0 = GetNextElement(enumerator, count);
+        item1 = GetNextElement(enumerator, count);
+        item2 = GetNextElement(enumerator, count);
+    }
+
+    public static void Deconstruct<T>(this IEnumerable<T> source, out T item0, out T item1, out T item2, out T item3)
+    {
+        const int count = 4;
+        using var enumerator = source.GetEnumerator();
+        item0 = GetNextElement(enumerator, count);
+        item1 = GetNextElement(enumerator, count);
+        item2 = GetNextElement(enumerator, count);
+        item3 = GetNextElement(enumerator, count);
     }
 
     private static T GetNextElement<T>(IEnumerator<T> enumerator, int expectedSize)
