0b6ff5fd9e99b9e76a14874f1a39b27fb60e3909
Dan <dan.mok999@yandex.ru>
2023-07-13 18:54:05 +0500

    CT.WaitForCancellation

---
+++ Kami.Utils.Tests/CancellationTokenExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Kami.Utils.Tests;
+
+[TestFixture]
+public class CancellationTokenExtensionsTests
+{
+    [Timeout(1000)]
+    [Test]
+    public async Task WaitForCancellation_Returns_WhenTokenCanceled()
+    {
+        var cts = new CancellationTokenSource();
+        var ct = cts.Token;
+
+        var task = ct.WaitForCancellation();
+        var sw = Stopwatch.StartNew();
+        _ = Task.Run(() =>
+        {
+            Thread.Sleep(100);
+            cts.Cancel();
+        });
+        await task;
+        sw.Stop();
+
+        Assert.That(sw.Elapsed, Is.GreaterThan(100.Milliseconds()));
+    }
+}
---
+++ Kami.Utils/CancellationTokenExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kami.Utils;
+
+public static class CancellationTokenExtensions
+{
+    public static Task WaitForCancellation(this CancellationToken cancellationToken)
+    {
+        var tcs = new TaskCompletionSource();
+        cancellationToken.Register(s => ((TaskCompletionSource)s!).SetResult(), tcs);
+        return tcs.Task;
+    }
+}
